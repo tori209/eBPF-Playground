34d57f23389766fe7b4909be21149fbe5d941571
tori209 <kyoungho2018@gist.ac.kr>
2025-03-24 08:29:09 +0000

    [fix] xdp_track.h의 주석 제거

--- network-track/xdp_track.h
+++ network-track/xdp_track.h
@@ -10,10 +10,8 @@ typedef struct http_event {
 	__u32 src_ip;
 	__u32 dst_ip;
 	__u32 pid;
-//	__u16 header_size;
 	char method[MAX_HTTP_METHOD_LENGTH];
 	char path[MAX_HTTP_PATH_LENGTH];
-//	char raw_header[MAX_HTTP_HEADER_SIZE];
 } ip_event_t;
 
 #endif /* __TC_TRACK_H_ */
